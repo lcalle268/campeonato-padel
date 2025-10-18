4a4dec43db7338b3c694e344ceaca6c1ff3fed7d
lcalle268 <callelopezlourdes@gmail.com>
2025-10-18 11:47:18 +0200

    Add files via upload

--- padel_app.py
+++ padel_app.py
@@ -8,18 +8,23 @@ Created on Fri Oct 17 13:13:11 2025
 import streamlit as st
 import pandas as pd
 
-st.title("🏆 Campeonato de Pádel - Oficina")
+st.set_page_config(page_title="Campeonato de Pádel", layout="wide")
+st.title("🏆 Campeonato de Pádel - SGFAL")
 
-# Leer los datos
+# Leer los datos desde el Excel
 partidos = pd.read_excel("padel.xlsx", sheet_name="partidos")
 clasificacion = pd.read_excel("padel.xlsx", sheet_name="clasificacion")
 
-# Seleccionar categoría
+# Selección de categoría
 categoria = st.selectbox("Selecciona categoría:", partidos["categoria"].unique())
 
-# Filtrar y mostrar
+# Mostrar resultados
 st.subheader("Resultados")
 st.dataframe(partidos[partidos["categoria"] == categoria])
 
+# Mostrar clasificación
 st.subheader("Clasificación")
-st.dataframe(clasificacion[clasificacion["categoria"] == categoria])
+clasif_filtrada = clasificacion[clasificacion["categoria"] == categoria].sort_values(
+    by="puntos", ascending=False
+)
+st.dataframe(clasif_filtrada)
